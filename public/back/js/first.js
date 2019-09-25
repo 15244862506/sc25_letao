8bf55eb45340b5de89a6dfb12078cde793a4aaf0
15244862506 <2320951610@qq.com>
2019-09-25 15:23:24 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -32,11 +32,62 @@ function render() {
           // 更新当前页
           currentPage = page;
           //重新渲染
-          render(); 
+          render();
         }
       })
     }
   })
 }
 
-})
+// 2.点击添加分类按钮 添加模态框
+  $('#addBtn').click(function () {
+      $('#addModal').modal("show");
+  });
+
+// 3.使用表单校验 ， 实现表单校验
+  $('#form').bootstrapValidator({
+
+    // 配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh' // 校验中
+    },
+    //配置字段
+    fields:{
+      categoryName: {
+        validators:{
+          notEmpty:{
+            message:"一级分类不能为空"
+          }
+        }
+      }
+    }
+  })
+
+  // 4.注册表单校验成功事件，阻止表单默认提交行为 通过ajax提交
+    $('#form').on('success.form.bv',function (e) {
+      e.preventDefault();
+
+
+      //通过ajax提交
+      $.ajax({
+        type: "post",
+        url: "/category/addTopCategory",
+        data: $('#form').serialize(),
+        dataType: "json",
+        success:function (info) {
+          console.log(info);
+          //添加完成
+          // 关闭模态框，页面重新渲染
+          $('#addModal').modal('hide');
+          // 2.页面重新渲染到第一页，让用户看到第一页数据
+          currentPage =1;
+          render();
+          //重置模态框
+          $('#form').data("bootstrapValidator").resetForm(true);
+        }
+      })
+    })
+
+});
