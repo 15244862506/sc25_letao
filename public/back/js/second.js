56f18b2d5dc71944a3436c78e9403f03e25d8cab
15244862506 <2320951610@qq.com>
2019-09-25 22:26:24 +0800

    二级分类，设置隐藏域进行提交

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -47,7 +47,55 @@ $(function() {
 
     $("#addBtn").click(function () {
       $('#addModal').modal('show');
+
+      //发送ajax请求 获取一级分类全部数据 通过模版引擎渲染
+      // 通过page=1，pagesize=100模拟获取全部分类数据的接口
+      $.ajax({
+          type: "get",
+        url: "/category/queryTopCategoryPaging",
+        data: {
+            page: 1,
+          pageSize:100
+        },
+        dataType:"json",
+        success:function (info) {
+          console.log(info);
+          //通过模版引擎渲染 结合模版和数据
+          var htmlStr = template('dropdwnTpl' ,info);
+          $('.dropdown-menu').html( htmlStr );
+        }
+      })
+    })
+
+  // 3.通过事件委托 给dropdown-menu下的所以a绑定点击事件
+    $('.dropdown-menu').on("click" ,'a' ,function () {
+    // 获取a的文本
+      var txt = $(this).text();
+      // 设置给dropdwomText
+      $('.dropdownText').text( txt );
+
+      //获取选中的id
+      var id = $(this).data("id");
+      //设置给input
+      $('[name="categoryId"]').val(id);
+
     })
 
 
+  // 4.进行文件上传初始化
+    $('#fileupload').fileupload({
+      // 配置返回到数据格式
+      dataType: "json",
+      //图片上传完成后会调用done回调函数
+      done:function (e , data) {
+        //获取上传 得到的图片地址
+        console.log(data.result.picAddr);
+        var imgUrl = data.result.picAddr;
+        // 赋值给img
+        $('#imgBox img').attr( "src",imgUrl)
+        //将图片地址 设置给input
+        $('[name = "brandLogo"]').val( imgUrl )
+      }
+
+    })
 });
