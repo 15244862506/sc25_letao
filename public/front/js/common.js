4f468f7b556a3e697e1f795f9916705f0c4caf6f
15244862506 <2320951610@qq.com>
2019-09-29 15:36:37 +0800

    封装-解析地址栏参数的方法的函数

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -8,3 +8,27 @@ var gallery = mui('.mui-slider');
 gallery.slider({
   interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
 });
+//该方法专门用于解析地址栏参数
+function getSearch(k) {
+  //获取地址栏 参数
+  var search = location.search;
+
+  // 将其解码成中文
+  search = decodeURI( search );
+
+  //去掉问号
+  search = search.slice(1);
+
+  //通过&将其分割成数组
+  var arr = search.split("&");
+
+  var obj = {};
+  arr.forEach(function (v ,i) {
+    var key = v.split("=")[0];
+    var value = v.split("+")[0];
+    //通过[]语法会解析变量
+    obj[key] = value;
+  })
+  return obj[k];
+}
+console.log(getSearch("k"));
